a975b20a272629729be96732e91feac02ee6d381
Jakson_Alves <jakson.junioralves1414@gmail.com>
2021-11-23 21:58:36 -0300

    Aula 23/11/2021

--- daw-loja/loja-react/src/App.tsx
+++ daw-loja/loja-react/src/App.tsx
@@ -1,30 +1,110 @@
+import { Collection } from "./components/Collection";
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
+import { Brands } from "./components/Brand";
+
+
 function App() {
+  const brands = [
+    {alt: 'brand1', source: '"assets/images/brand1.png"'},
+    {alt: 'brand2', source: '"assets/images/brand2.png"'},
+    {alt: 'brand3', source: '"assets/images/brand3.png"'},
+    {alt: 'brand4', source: '"assets/images/brand4.png"'},
+    {alt: 'brand5', source: '"assets/images/brand5.png"'}
+  ];
+  
   return (
     <div>
-      <header className="navbar navbar-expand navbar-light">
+
+      <Header />
+
+      <main>
         <div className="container">
-          <h1 className="navbar-brand">DAW Loja</h1>
-          <nav>
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <a className="nav-link active" href="#">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Produtos</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Sobre</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Contato</a>
-              </li>
-            </ul>
-          </nav>
+
+          <Collection />
+
+          <div className="products">
+            <div className="card">
+              <img src="assets/images/product1.jpg" />
+              <div className="card-body">
+                <h5>Conjunto de casaco e calça preta</h5>
+                <div className="details">
+                  <div className="star-rating">
+                    <i className="bi bi-star-fill"></i>
+                    <i className="bi bi-star-fill"></i>
+                    <i className="bi bi-star-fill"></i>
+                    <i className="bi bi-star"></i>
+                    <i className="bi bi-star"></i>
+                  </div>
+                  <div className="price">
+                    <h3>R$600,00</h3>
+                    <p>10 x R$ 60,00</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="card">
+              <img src="assets/images/product2.jpg" />
+              <div className="card-body">
+                <h5>Conjunto de casaco e calça preta</h5>
+                <div className="details">
+                  <div className="star-rating">
+                    <i className="bi bi-star-fill"></i>
+                    <i className="bi bi-star-fill"></i>
+                    <i className="bi bi-star-fill"></i>
+                    <i className="bi bi-star-fill"></i>
+                    <i className="bi bi-star"></i>
+                  </div>
+                  <div className="price">
+                    <h3>R$600,00</h3>
+                    <p>10 x R$ 60,00</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="card">
+              <img src="assets/images/product3.jpg" />
+              <div className="card-body">
+                <h5>Conjunto de casaco e calça preta</h5>
+                <div className="details">
+                  <div className="star-rating">
+                    <i className="bi bi-star-fill"></i>
+                    <i className="bi bi-star-fill"></i>
+                    <i className="bi bi-star-fill"></i>
+                    <i className="bi bi-star-fill"></i>
+                    <i className="bi bi-star-fill"></i>
+                  </div>
+                  <div className="price">
+                    <h3>R$600,00</h3>
+                    <p>10 x R$ 60,00</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </header>
+
+        <div className="newsletter">
+          <div className="container">
+            <h2>Newsletter</h2>
+            <p className="fw-light">Assine nossa Newsletter para receber atualizações sobre ofertas especiais, novos produtos ou informações
+              de descontos</p>
+            <form>
+              <input type="text" className="form-control form-control-lg" placeholder="Informe o seu e-mail" />
+              <button className="btn btn-primary btn-lg ">Inscreva-se</button>
+            </form>
+          </div>
+        </div>
+
+        <div className="container">
+          <Brands items={brands}/>
+        </div>
+      </main>
+
+      <Footer />
+
     </div>
   );
 }
-
 
 export default App;
